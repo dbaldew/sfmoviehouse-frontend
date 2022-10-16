bd3f0bb48dcbef888bf7492a01512ed602232a26
dbaldew <dbaldew@gmail.com>
2022-10-16 22:11:11 +0200

    moviecard

---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,21 @@
+import "./MovieCard.css";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+function MovieCard() {
+    
+
+
+
+    return (
+        <>
+            <div>
+                <p>test</p>
+
+            </div>
+
+        </>
+    )
+}
+
+export default MovieCard;
