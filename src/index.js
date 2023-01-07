d95a55e21663f0389abfee259808fb51afaa2472
dbaldew <dbaldew@gmail.com>
2023-01-07 09:35:47 +0100

    removed MovieDataContext

--- src/index.js
+++ src/index.js
@@ -5,16 +5,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthContextProvider from "./context/AuthContext";
-import MovieDataContextProvider from "./context/MovieDataContext";
+
 
 
 ReactDOM.render(
     <React.StrictMode>
         <Router>
             <AuthContextProvider>
-                <MovieDataContextProvider>
                     <App/>
-                </MovieDataContextProvider>
             </AuthContextProvider>
         </Router>
     </React.StrictMode>,
